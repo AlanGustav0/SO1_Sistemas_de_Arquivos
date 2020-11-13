3c438f86ba4070adcb5e998d0fe3ba768ccae358
Alan Gustavo <alangustavo946@gmail.com>
2020-11-13 07:49:15 -0300

    2º - Commit - Metodos verificaCadastro e imprimeCadastro

--- src/controller/ArquivosController.java
+++ src/controller/ArquivosController.java
@@ -1,7 +1,12 @@
 package controller;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 import javax.swing.JOptionPane;
 
@@ -10,13 +15,13 @@ public class ArquivosController implements IArquivosController {
 	@Override
 	public void verificaDirTemp() throws IOException {
 		File caminho = new File("C:\\TEMP");
-		if(caminho.exists() && caminho.isDirectory()){
-			JOptionPane.showMessageDialog(null,"ERRO - O diretório já existe!.");
-		}else {
+		if (caminho.exists() && caminho.isDirectory()) {
+			JOptionPane.showMessageDialog(null, "O diretório já existe!.");
+		} else {
+			JOptionPane.showMessageDialog(null, "Diretório Criado com sucesso.");
 			caminho.mkdir();
 		}
-		
-		
+
 	}
 
 	@Override
@@ -27,14 +32,79 @@ public class ArquivosController implements IArquivosController {
 
 	@Override
 	public void imprimeCadastro(String arquivo, int codigo) throws IOException {
-		// TODO Auto-generated method stub
-		
+
+		File arq = new File(arquivo);
+		String[] conteudoArquivo;
+
+		if (verificaCadastro(arquivo, codigo)) {
+			FileInputStream fluxo = new FileInputStream(arq);
+			InputStreamReader leitor = new InputStreamReader(fluxo);
+			BufferedReader buffer = new BufferedReader(leitor);
+
+			String linha = buffer.readLine();
+
+			
+			while (linha != null) {
+
+				if (linha.contains(Integer.toString(codigo))) {
+					conteudoArquivo = linha.split(";");
+					for (int i = 0; i < conteudoArquivo.length; i++) {
+						if (i == 0) {
+							System.out.println("Código: " + conteudoArquivo[i]);
+						} else if (i == 1) {
+							System.out.println("Nome: " + conteudoArquivo[i]);
+						} else {
+							System.out.println("E-mail: " + conteudoArquivo[i]);
+						}
+					}
+				}
+
+				linha = buffer.readLine();
+			}
+
+			buffer.close();
+			leitor.close();
+			fluxo.close();
+
+		}
+
 	}
 
 	@Override
 	public void insereCadastro(String arquivo, int codigo, String nome, String email) throws IOException {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	private boolean verificaCadastro(String arquivo, int codigo) throws IOException {
+
+		File arq = new File(arquivo);
+		String linha;
+		String codigoArquivo = Integer.toString(codigo);
+
+		if (arq.exists() && arq.isFile()) {
+			FileInputStream fluxo = new FileInputStream(arq);
+			InputStreamReader leitor = new InputStreamReader(fluxo);
+			BufferedReader buffer = new BufferedReader(leitor);
+
+			linha = buffer.readLine();
+
+			while (linha != null) {
+
+				if (linha.contains(codigoArquivo)) {
+					return true;
+				}
+
+				linha = buffer.readLine();
+			}
+
+			buffer.close();
+			leitor.close();
+			fluxo.close();
+		}
+
+		return false;
+
 	}
 
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -10,9 +10,12 @@ public class Principal {
 	public static void main(String[] args) {
 
 		IArquivosController arquivosCont = new ArquivosController();
+		String nome = "arquivo.csv";
+		int codigo = 235;
 
 		try {
 			arquivosCont.verificaDirTemp();
+			arquivosCont.imprimeCadastro(nome,codigo);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
